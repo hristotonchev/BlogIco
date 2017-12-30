83eb5e7316b027c27f88e4aa7f7eb6280aac8975
hristotonchev <hristotonchev1991@gmail.com>
2017-12-30 19:46:19 +0200

    master comments in admin and git ignore file

--- inc/header_admin.php
+++ inc/header_admin.php
@@ -1,13 +1,16 @@
+<?php include("../admin/session.php"); ?>
 <link href="../css/styles2.css" rel="stylesheet" type="text/css" />
 <?php
-include_once("dbconfig.php");
-include("session.php");
+include_once("../admin/dbconfig.php");
 $currentPage = '';
 
 ?>
 <a href="admin.php"   class='button <?php if($currentPage == 'admin'){ echo 'active';}?>'>CREATE BLOG</a>
 
 <a href="blogposts.php" class='button <?php if($currentPage == 'blogposts'){ echo 'active';}?>'>BLOG POSTS</a>
+
+<a href="comment_list.php" class='button <?php if($currentPage == 'comment_list'){ echo 'active';}?>'>COMMENTS</a>
+
 
 <a href='logout.php' class='button button4'>LOGOUT</a>
 
